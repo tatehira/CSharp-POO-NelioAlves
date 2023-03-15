7ea33e04a22eea9a9354dee5f68078902e52a13d
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-15 16:08:36 -0400

    Adicionaddo Udate POO

---
+++ 2-C#POO/2- ExercicioPOO/Ex-001-Triangulo/Ex-001/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.Write("Entre com as medidas do triangulo X: ");
+            xA = double.Parse(Console.ReadLine());
+            xB = double.Parse(Console.ReadLine());
+            xC = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Entre com as medidas do triangulo y: ");
+            yA = double.Parse(Console.ReadLine());
+            yB = double.Parse(Console.ReadLine());
+            yC = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine($"Medida do triangulo X: {xA}, {xB} E {xC}");
+            Console.WriteLine($"Medida do triangulo y: {yA}, {yB} E {yC}");
+        }
+    }
+}
---
+++ 2-C#POO/2- ExercicioPOO/Ex-001-Triangulo/Ex-001/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_001
+{
+    internal class Triangulo
+    {
+        public double A; // Public: Pode ser acessado de outro arquivo
+        public double B;
+        public double C;
+    }
+}
