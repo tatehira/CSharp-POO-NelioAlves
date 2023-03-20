4d3dec58e823c15e036617688b41650b3d455aa7
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-20 16:28:43 -0400

    Adicionado exercicio sobrecarga

---
+++ 2-C#POO/2- ExercicioPOO/Ex-011-Sobrecarga/Ex-011-Sobrecarga/Produto.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade) // CONSTRUTOR QUE RECEBE OBRIGATORIO OS VALORES DOS PARAMETROS.
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) // Adicionado sobrecarga que alem de receber o parametro obrigatorio, seta o valor quantidade default.
+        { 
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 10;
+        }
+
+        public Produto() // terceira opção para sobrecarga sem obrigatoriedade nos valores - tendo 3 opções, podendo usar as 3 simultaneas
+        {
+
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 2-C#POO/2- ExercicioPOO/Ex-011-Sobrecarga/Ex-011-Sobrecarga/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sobrecarga é um recurso que uma classe de oferecer mais de uma operacao com o mesmo nome, porém com diferente listas de parametros.
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Clear();
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Produto p = new Produto(nome, preco, quantidade); // Construtor recebendo os dados como parametro
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Clear();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.Clear();
+
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
