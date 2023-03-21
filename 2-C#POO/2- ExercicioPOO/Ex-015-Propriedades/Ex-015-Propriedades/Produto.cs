f37c6034425e042bd51bc937d779edfd9691dcfd
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-21 18:31:05 -0400

    Propriedade

---
+++ 2-C#POO/2- ExercicioPOO/Ex-015-Propriedades/Ex-015-Propriedades/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace Ex_014_Encapsulamento
+{
+    class Produto
+    { // Exemplo de encapsulamento. Quando faço isso, não é possível enxergar ele de outra classe.
+      // Deve-se criar um método para inserir os valores
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        #region Sobrecarga
+        // CONSTRUTOR QUE RECEBE OBRIGATORIO OS VALORES DOS PARAMETROS. Os parametros poderão ser de outra tela
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        #endregion Sobrecarga
+
+        #region Propriedades usando { Get; Set; }
+        //Com esse método, podemos acessar atravez da instancia da classe
+        // Assim, estamos ordenando o que pode ser buscando. Pois está sendo passado dentro do get e set
+        public string Nome 
+        {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 4) // O value é o valor que está sendo setado no set
+                    _nome = value;
+            }
+        }
+
+        public double preco
+        {
+            get { return _preco; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+        #endregion Propriedades
+    }
+}
---
+++ 2-C#POO/2- ExercicioPOO/Ex-015-Propriedades/Ex-015-Propriedades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Ex_014_Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Explicação
+            /* 1- São definições de métodos encapsulados, porém expondo uma sintaxe similar á de atributos e não de métodos
+               2- uma propriedade é um membro que podemos: Gravar, ler ou calcular o valor de um campo
+               3- Podem ser usadas como se fossem atributos publicos. Más são métodos epeciais chamados de Acessadores
+               4- Ela permite que dados sejam acessados e possui segurança e flexibilidade*/ 
+            #endregion Explicação
+
+            string produto = "TV";
+            double valor = 500.00;
+            int qnt = 10;
+
+            Produto p = new Produto(produto, valor, qnt);
+
+            // setando valores na instancia privada encapsulada
+            p.Nome = "TV";
+
+            // Chamandos os campos
+            Console.WriteLine(p.Nome);
+
+            Console.WriteLine(p.preco);
+
+            Console.WriteLine(p.Quantidade);
+        }
+    }
+}
