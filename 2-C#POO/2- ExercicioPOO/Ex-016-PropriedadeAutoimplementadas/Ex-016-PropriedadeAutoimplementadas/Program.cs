80c0ac34a46f09bafcf7dc67c20a541dbee41804
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-22 14:04:53 -0400

    Update

---
+++ 2-C#POO/2- ExercicioPOO/Ex-016-PropriedadeAutoimplementadas/Ex-016-PropriedadeAutoimplementadas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Ex_016_PropriedadeAutoimplementadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region EXPLICAÇÃO
+            /*
+            - É uma forma simplificada de se declarar propriedades
+            - Não necessetam de lógica particulares para as operações get e set
+
+            public int MyProperty {get; set;}
+            */
+            #endregion EXPLICAÇÃO
+
+            public int Quantiade { get; set; }
+            
+        }
+    }
+}
