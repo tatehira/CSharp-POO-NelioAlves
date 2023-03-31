538b4b5932d4ae65fa258592ac2a7c79f5ee79b5
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-31 17:15:25 -0400

    update

---
+++ 2-C#POO/2- ExercicioPOO/3-AlocMemory-Arrays-Listas/Ex-029-Exercicio-02-Matriz/Ex-029-Exercicio-02-Matriz/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex_029_Exercicio_02_Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
