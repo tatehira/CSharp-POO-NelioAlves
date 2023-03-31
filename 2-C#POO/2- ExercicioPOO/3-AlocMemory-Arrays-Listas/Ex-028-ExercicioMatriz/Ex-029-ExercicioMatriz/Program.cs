d5867174d44e097e5d33b503094bd770e29cf7d8
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-03-31 17:05:59 -0400

    update Exercicio-01 matriz

---
+++ 2-C#POO/2- ExercicioPOO/3-AlocMemory-Arrays-Listas/Ex-028-ExercicioMatriz/Ex-029-ExercicioMatriz/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Ex_028_ExercicioMatriz
+{
+    internal class Program
+    {
+        /*                                          EXERCICIO
+         * Fazer um exercicio para  ler um número inteiro N e uma matriz de ordem N contendo numeros inteiros.
+         * Em seguida, mostrar a diagonal principal e a quantiadede de valores negativos da matriz;
+         */
+        static void Main(string[] args)
+        {
+
+            #region Criação da matriz
+            Console.Write("Digite o numero de linhas e colunas da matriz: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[num, num];
+
+            for(int i = 0; i< num; i++)
+            {
+                string[] VetorMatriz = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < num; j++)
+                {
+                    matriz[i, j] = int.Parse(VetorMatriz[j]);
+                }
+            }
+            #endregion Criação da matriz
+
+            #region Calculando Numero negativo
+
+            double CalculoNegativo = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    if (matriz[i,j] < 0)
+                    CalculoNegativo ++;
+                }
+            }
+
+            Console.WriteLine($"Possui {CalculoNegativo} números negativos!");
+
+            #endregion Calculando Numero negativo
+
+            #region Mostrar na tela os dados da matriz
+
+            Console.WriteLine("Os números da matriz são: ");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion Mostrar na tela os dados da matriz
+
+        }
+    }
+}
