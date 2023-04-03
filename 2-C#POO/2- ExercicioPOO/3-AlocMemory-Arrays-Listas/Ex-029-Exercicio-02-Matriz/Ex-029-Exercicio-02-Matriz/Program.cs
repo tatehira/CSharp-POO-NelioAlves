779e4a535eeb4e898d321deb002c46f398fbe27a
tatehira <niickcostta00@outlook.com>
2023-04-03 01:53:38 -0400

    update matriz

--- 2-C#POO/2- ExercicioPOO/3-AlocMemory-Arrays-Listas/Ex-029-Exercicio-02-Matriz/Ex-029-Exercicio-02-Matriz/Program.cs
+++ 2-C#POO/2- ExercicioPOO/3-AlocMemory-Arrays-Listas/Ex-029-Exercicio-02-Matriz/Ex-029-Exercicio-02-Matriz/Program.cs
@@ -12,7 +12,7 @@
             Console.Write("Digite o numero de Colunas da matriz: ");
             int Colunas = int.Parse(Console.ReadLine());
 
-            int[,] Matriz = new int[Linhas, Colunas];
+            int[,] Matriz = new int[Linhas, Colunas]; // criando matriz
 
             int equal = 0;
             Console.Write("Digite os numeros: ");
@@ -43,24 +43,87 @@
 
             #endregion Construção da matriz
 
+
             #region Selecionando Posições
+            Console.WriteLine();
+            Console.WriteLine("Busque um elemento: ");
+            Console.Write("Selecione um número para buscar: ");
+            int FindNumber = int.Parse(Console.ReadLine());
 
-            //Console.Write("Selecione uma posição da LINHA para consulta: ");
-            //int PosicaoLinha = int.Parse(Console.ReadLine());
-
-            //Console.Write("Selecione uma posição da COLUNA para consulta: ");
-            //int PosicaoColuna = int.Parse(Console.ReadLine());
+            int PosicaoLinha = 0;
+            int PosicaoColuna = 0;
+            int Acima = 0;
+            int Abaixo = 0;
+            int Esquerda = 0;
+            int Direita = 0;
 
             for (int i = 0; i < Matriz.GetLength(0); i++)
             {
                 for (int j = 0; j < Matriz.GetLength(1); j++)
                 {
-                    //if( )
-                    //{
+                    if (Matriz[i, j] == FindNumber)
+                    {
+                        PosicaoLinha = i + 1;
+                        PosicaoColuna = j + 1;
+                        Console.WriteLine($"O numero {FindNumber} está em [{PosicaoLinha}, {PosicaoColuna}] ");
+                        
+                        Direita = Matriz[i, j + 1];
+                        if (Direita >= 0)
+                        { 
+                            Console.WriteLine("Direita: " + Direita);
+                        }
 
-                    //}
+                        Esquerda = Matriz[i, j - 1];
+                        if (Esquerda >= 0)
+                        {
+                            Console.WriteLine("Esquerda: " + Esquerda);
+                        }
+
+                        Acima = Matriz[i - 1, j];
+                        if (Acima>= 0)
+                        {
+                            Console.WriteLine("Acima: " + Acima);
+                        }
+
+                        Abaixo = Matriz[i + 1, j];
+                        if (Abaixo >= 0)
+                        {
+                            Console.WriteLine("Abaixo: " + Abaixo);
+                        }
+                    }
+
+                    if (Matriz[i, j] == FindNumber)
+                    {
+                        PosicaoLinha = i + 1;
+                        PosicaoColuna = j + 1;
+                        Console.WriteLine($"O numero {FindNumber} está em [{PosicaoLinha}, {PosicaoColuna}] ");
+
+                        Direita = Matriz[i, j + 1];
+                        if (Direita >= 0)
+                        {
+                            Console.WriteLine("Direita: " + Direita);
+                        }
+
+                        Esquerda = Matriz[i, j - 1];
+                        if (Esquerda >= 0)
+                        {
+                            Console.WriteLine("Esquerda: " + Esquerda);
+                        }
+
+                        Acima = Matriz[i - 1, j];
+                        if (Acima >= 0)
+                        {
+                            Console.WriteLine("Acima: " + Acima);
+                        }
+
+                        Abaixo = Matriz[i + 1, j];
+                        if (Abaixo >= 0)
+                        {
+                            Console.WriteLine("Abaixo: " + Abaixo);
+                        }
+                    }
                 }
-            }
+            }           
 
             #endregion
         }
