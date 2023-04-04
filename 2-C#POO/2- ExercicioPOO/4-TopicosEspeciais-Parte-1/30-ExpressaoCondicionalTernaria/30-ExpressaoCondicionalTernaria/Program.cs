531532501083f57d630da7c26d683ddfa6fcfad9
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-04-04 14:36:01 -0400

    Condicional ternaria

---
+++ 2-C#POO/2- ExercicioPOO/4-TopicosEspeciais-Parte-1/30-ExpressaoCondicionalTernaria/30-ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _30_ExpressaoCondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
