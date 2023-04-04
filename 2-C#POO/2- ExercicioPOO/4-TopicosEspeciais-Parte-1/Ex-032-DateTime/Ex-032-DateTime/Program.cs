a5f0767ca5e5fbb50e835b10d6092f97f3dc11a7
Wendrew WN. Nick Costa Tatehira <wendrew.nick@liberali.local>
2023-04-04 15:42:47 -0400

    Prpriedade datetime

--- 2-C#POO/2- ExercicioPOO/4-TopicosEspeciais-Parte-1/Ex-032-DateTime/Ex-032-DateTime/Program.cs
+++ 2-C#POO/2- ExercicioPOO/4-TopicosEspeciais-Parte-1/Ex-032-DateTime/Ex-032-DateTime/Program.cs
@@ -32,10 +32,14 @@ namespace Ex_032_DateTime
             */
             #endregion Instanciação
 
-            DateTime dateTime = DateTime.Parse("25/07/2000");
-            DateTime dateTime2 = DateTime.Parse("25/07/2000 10:02:34");
-
-            DateTime dateTime3 = DateTime.ParseExact("15/08/2000 13:00:03", " dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+			DateTime d1 = DateTime.Parse("2000-08-15");
+			DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
+			DateTime d3 = DateTime.Parse("15/08/2000");
+			DateTime d4 = DateTime.Parse("15/08/2000 13:05:58");
+			Console.WriteLine(d1);
+			Console.WriteLine(d2);
+			Console.WriteLine(d3);
+			Console.WriteLine(d4);
 
         }
     }
